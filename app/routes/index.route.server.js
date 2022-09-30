31b929067c0a77a0b81eeecb9bddd0c26361dd21
Salma Chaaban <schaaban@my.centennialcollege.ca>
2022-09-30 07:06:23 +0300

    views and partials completed

--- app/routes/index.route.server.js
+++ app/routes/index.route.server.js
@@ -1,12 +1,16 @@
 // Index Routes
 import { Router } from 'express';
-import { helloWorld } from '../controllers/index.controller.server.js';
+import { diplayHomePage, displayAboutPage, displayContactPage, displayProjectsPage, displayServicesPage } from '../controllers/index.controller.server.js';
 
 // Instantiating the router
 const router = Router();
 
 // add middleware to connect application
-router.get('/',helloWorld);
-router.get('/hello',helloWorld);
+router.get('/',diplayHomePage);
+router.get('/home',diplayHomePage);
+router.get('/about', displayAboutPage);
+router.get('/contact', displayContactPage);
+router.get('/projects', displayProjectsPage);
+router.get('/services', displayServicesPage);
 
 export default router;
